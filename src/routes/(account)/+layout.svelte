4f61eae03b9cabdae54fb76a2cb83e963ec93694
Jack Kufa <jack@kufa.io>
2023-09-29 17:29:02 -0400

    Change to svh

--- src/routes/(account)/+layout.svelte
+++ src/routes/(account)/+layout.svelte
@@ -13,7 +13,7 @@
 		max-width: 800px;
 		background-color: #fcfcfc;
 		display: flex;
-		height: 100vh;
+		height: 100svh;
 		justify-content: center;
 		margin: auto;
 		// box-shadow: 8px 0px 16px 0px rgba(10, 5, 37, 0.05), 0px 0px 4px 0px rgba(10, 5, 37, 0.04);
