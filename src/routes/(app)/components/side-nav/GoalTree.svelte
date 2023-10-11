d437a6d7e16c055240c1be6b98ff05f3666335cf
Jack Kufa <jack@kufa.io>
2023-10-11 16:35:56 -0400

    Update goalTree to do indentation on the fly

--- src/routes/(app)/components/side-nav/GoalTree.svelte
+++ src/routes/(app)/components/side-nav/GoalTree.svelte
@@ -1,24 +1,50 @@
 <script lang="ts">
 	import type { Goal } from '$lib/types/sb';
+	import { beforeUpdate, onMount } from 'svelte';
+	import NavItem from './NavItem.svelte';
 	export let goals: Goal[] = [];
+
+	let idToParent = new Map();
+
+	beforeUpdate(() => {
+		goals.forEach((goal) => {
+			idToParent.set(goal.id, goal.parent_id);
+		});
+	});
+
+	function traceLineage(parent_id: number | null, depth = 0) {
+		if (parent_id === null) {
+			return depth;
+		}
+		return traceLineage(idToParent.get(parent_id), depth + 1);
+	}
 </script>
 
-<ul class="goals-list">
-	{#each goals as goal}
-		<slot {goal} />
-		{#if goal.children}
-			<!-- TODO: Figure out why this let:goal causes eslint to freeze -->
-			<svelte:self let:goal goals={goal.children}>
-				<slot {goal} />
-			</svelte:self>
-		{/if}
-	{/each}
+<ul class="goals-list" data-testid="goal-tree">
+	{#if idToParent.size !== 0}
+		{#each goals as goal}
+			<li style="margin-left: {traceLineage(goal.parent_id) * 1.5}rem">
+				<NavItem href="/goals/{goal.id}">
+					<span slot="text" class="goal-name">{goal.name}</span>
+				</NavItem>
+			</li>
+		{/each}
+	{/if}
 </ul>
 
 <style lang="scss">
 	ul.goals-list {
 		margin: 0;
 		list-style: none;
-		padding-inline-start: 1.5rem;
+		padding-inline-start: 0;
+	}
+
+	.goal-name {
+		word-wrap: break-word;
+		word-break: break-all;
+		overflow: hidden;
+		display: -webkit-box;
+		-webkit-line-clamp: 1;
+		-webkit-box-orient: vertical;
 	}
 </style>
---
+++ src/routes/(app)/components/side-nav/GoalTree.test.ts
@@ -0,0 +1,91 @@
+import { describe, expect, it } from 'vitest';
+import { render } from '@testing-library/svelte';
+import GoalTree from './GoalTree.svelte';
+
+describe('GoalTree', () => {
+	const data = [
+		{
+			id: 0,
+			user_id: 'f7af2121-ef15-4ae4-8eb4-0c0c94e71b57',
+			name: 'Reach financial independence',
+			index: 0,
+			created_at: '2023-10-04T01:16:15.153696+00:00',
+			updated_at: null,
+			target_date: null,
+			completed_at: null,
+			completed: false,
+			description: null,
+			parent_id: null
+		},
+		{
+			id: 1,
+			user_id: 'f7af2121-ef15-4ae4-8eb4-0c0c94e71b57',
+			name: 'Build an emergency fund of 6 months of expenses',
+			index: 1,
+			created_at: '2023-10-04T01:25:06.894879+00:00',
+			updated_at: null,
+			target_date: null,
+			completed_at: null,
+			completed: false,
+			description: null,
+			parent_id: 0
+		},
+		{
+			id: 2,
+			user_id: 'f7af2121-ef15-4ae4-8eb4-0c0c94e71b57',
+			name: 'Create a budget',
+			index: 2,
+			created_at: '2023-10-04T02:13:58.992435+00:00',
+			updated_at: null,
+			target_date: null,
+			completed_at: null,
+			completed: false,
+			description: null,
+			parent_id: 0
+		},
+		{
+			id: 3,
+			user_id: 'f7af2121-ef15-4ae4-8eb4-0c0c94e71b57',
+			name: 'Save aggressively for the next 2 months',
+			index: 3,
+			created_at: '2023-10-04T02:14:56.540889+00:00',
+			updated_at: null,
+			target_date: null,
+			completed_at: null,
+			completed: false,
+			description: null,
+			parent_id: 1
+		},
+		{
+			id: 4,
+			user_id: 'f7af2121-ef15-4ae4-8eb4-0c0c94e71b57',
+			name: 'test',
+			index: 4,
+			created_at: '2023-10-11T01:20:04.154255+00:00',
+			updated_at: null,
+			target_date: null,
+			completed_at: null,
+			completed: false,
+			description: 'woo',
+			parent_id: null
+		},
+		{
+			id: 5,
+			user_id: 'f7af2121-ef15-4ae4-8eb4-0c0c94e71b57',
+			name: 'Oct 10 goal',
+			index: 5,
+			created_at: '2023-10-11T02:07:13.604382+00:00',
+			updated_at: null,
+			target_date: null,
+			completed_at: null,
+			completed: false,
+			description: null,
+			parent_id: null
+		}
+	];
+	it('should be created', () => {
+		const component = render(GoalTree, { goals: data });
+
+		expect(component).toBeTruthy();
+	});
+});
