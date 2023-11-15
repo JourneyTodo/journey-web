8fc8df61ef20c2c6d1140810b8fc665b37bcb6dd
Jack Kufa <jack@kufa.io>
2023-11-14 20:13:05 -0500

    Remove action menu component

--- src/lib/components/ActionMenu.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Button from './Button.svelte';
-</script>
-
-<div class="container">
-	<Button slot="items" size="small" variant="secondary" action="destructive" fill>Delete</Button>
-</div>
